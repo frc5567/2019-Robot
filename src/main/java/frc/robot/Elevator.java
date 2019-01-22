6f77fba450617bf8bc2a7e4c009dbf0bc7395d1b
matthewbogs711 <mattb3350@yahoo.com>
2019-01-22 17:31:21 -0500

    Modified Elevator.java class

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -5,18 +5,29 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedController;
 
 public class Elevator {
     // Defining the limit switches at the top and bottom of the elevator.
     DigitalInput m_limitTop;
     DigitalInput m_limitBottom;
 
-    // This constructor is initializing in creating a new instince of an elevator with limit port switch definitions.
+    // Declaring the encoder for the elevator height.
+    Encoder elevatorEncoder;
+
+    // Declaring the speed controller for the elevator.
+    SpeedController raiseElevatorMotor;
+
+    // This constructor is initializing in creating a new instance of an elevator with limit port switch definitions.
     Elevator(int topLimitPort, int bottomLimitPort){
-        // Creating a new instince of DigitalInput with the assigned port number.
+
+        // Creating a new instance of DigitalInput with the assigned port number.
         m_limitTop = new DigitalInput(topLimitPort);
         m_limitBottom = new DigitalInput(bottomLimitPort);
+    }    
+        // Instantiating encoder for the elevator height
+        elevatorEncoder = new Encoder(2, 1, false, EncodingType.k1X);
 
-    }
+    
 }
