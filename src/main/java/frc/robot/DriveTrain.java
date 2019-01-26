0a97265ed9da306b712902ba23fddc5b923622b6
HannahStrimpel <h4rescue@gmail.com>
2019-01-26 11:21:49 -0500

    Added DriveTrain

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -4,18 +4,41 @@
 
 package frc.robot;
 
-//we imported the methods for the controller, differential drive, and VictorSP
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive; 
+//importing the types (speed control, drive, and spark)
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.VictorSP;
+
+//we imported the methods for the differential drive
 
 
 public class DriveTrain {
-    private double m_motorPowerLeft= 0;
-    private double m_motorPowerRight= 0;
-    private XboxController m_driveController;
-    private VictorSP m_leftMotor;
-    private VictorSP m_rightMotor;
-    private DifferentialDrive m_driveTrain;
+    // Declare variable, and set numeric values
+    //We'll eventually make one of the Victors a Talon SRX and have it lead the other
+    //maybe clarify that m_frontLeft is a Victor or Talon
+    VictorSP m_frontLeft = new VictorSP(0);
+    VictorSP m_backLeft = new VictorSP(1);
+    SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_backLeft);
 
+    VictorSP m_frontRight = new VictorSP(2);
+    VictorSP m_backRight = new VictorSP(3);
+    SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_backRight);
+
+    DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
+    
+
+  
+
+    //Declaring drivetrain Speed controllers
+ 
+    //Declaring speed controller group
+
+
+    //Assign variable values
+ 
+    //Set left & right joystick values to motor power left & motor power right variables
+ 
+
+    //Set drive train power to motor variables
+*/
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,13 +5,35 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 
 public class Robot extends TimedRobot {
 
   Elevator m_elevator;
+  XboxController pilotDrive;
+    
+  double value; 
+  double stickValueLeft; 
+  double stickValueRight;
 
- 
+  SpeedController frontLeftMotor;
+  SpeedController backLeftMotor;
+  SpeedController frontRightMotor;
+  SpeedController backRightMotor;
+
+  //making the moters into two groups, right and left
+  SpeedControllerGroup leftSide = new SpeedControllerGroup (frontLeftMotor, backLeftMotor);
+  SpeedControllerGroup RightSide =new SpeedControllerGroup (frontRightMotor, backRightMotor);
+  DifferentialDrive myDrive = new DifferentialDrive (leftSide, RightSide);
+  Robot() {
+    pilotDrive = new XboxController(0);
+  }
+
   @Override
   public void robotInit() {
   
@@ -35,9 +57,40 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() {
+  public void teleopInit() {
+
+    
+ 
+
+ 
+
   }
 
+  @Override
+  public void teleopPeriodic() {
+
+    //This makes the deadbands, pilot control is our variable for user input
+    if((pilotDrive.getY(Hand.kLeft)<0.05) && (pilotDrive.getY(Hand.kLeft)>-0.05)){
+      stickValueLeft = 0;
+      
+    } else {
+      stickValueLeft = pilotDrive.getY(Hand.kLeft);
+    }
+
+    if((pilotDrive.getY(Hand.kRight)<0.05) && (pilotDrive.getY(Hand.kRight)>-0.05)){
+      stickValueRight = 0;
+      
+    } else {
+      stickValueLeft = pilotDrive.getY(Hand.kRight);
+    }    
+    //We'll eventually need to inverone of the controls
+   
+    myDrive.tankDrive(stickValueLeft, stickValueRight);
+
+    
+  }
+  //Quin should get more information on Talon->Victor pairs for 
+  //the final motor controllers
  
   @Override
   public void testPeriodic() {
