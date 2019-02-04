8c529ed028df29ccaeeb63159caf61e1669c3ea0
Josh <josh.overbeek@outlook.com>
2019-02-04 18:36:40 -0500

    Updated commenting for legibility

--- src/main/java/frc/robot/DuinoToRioComms.java
+++ src/main/java/frc/robot/DuinoToRioComms.java
@@ -23,9 +23,10 @@ public class DuinoToRioComms {
      * @return  Degrees to target. Will return Double.NaN if it fails to read properly.
      */
     public double getDegToTarget() {
+        //  Declares and instantiates a member value for storing the return from pixyRead
         Double m_degToTarget = Double.NaN;
 
-        //  
+        //  Calls pixyRead with the command 2 to get deg to target and assign it to return variable
         m_degToTarget = pixyRead(2);
 
         return m_degToTarget;
@@ -36,9 +37,10 @@ public class DuinoToRioComms {
      * @return  Distance to target (in). Will return Double.NaN if it fails to read properly.
      */
     public double getDistToTarget() {
+        //  Declares and instantiates a member value for storing the return from pixyRead
         Double m_distToTarget = Double.NaN;
 
-        //  
+        //  Calls pixyRead with the command 2 to get dist to target and assign it to return variable
         m_distToTarget = pixyRead(1);
 
         return m_distToTarget;
@@ -49,7 +51,7 @@ public class DuinoToRioComms {
      * @param command The value of the command requested, where 0 requests degreesToTarget, 1 requests dist to target
      */
     private double pixyRead(int command) {
-        
+        //  Declares and instantiates a member variable for storing return from readData        
         Double m_dataReturned = Double.NaN;
 
         //  Telemetry for testing communication: Print on enter to check for run
@@ -59,6 +61,7 @@ public class DuinoToRioComms {
         sendCommand(command);
         m_dataReturned = readData(command);
 
+        //  Telemetry for checking if the returned data was valid
         if(m_dataReturned.isNaN()){
             System.out.println("Nothing Returned");
         }
@@ -87,14 +90,15 @@ public class DuinoToRioComms {
      * @return A double parsed from the string passed by the Duino
      */
     private double readData(int command) {
+        //  Declares and instantiates a member variable for storing return from arduino        
         Double m_dataDouble = Double.NaN;
 
         //  Allocates recieved data to a string
         String m_sPixyOut = m_duinoPort.readString();
 
+        //  Checks to see if the passed in command is valid
         if ( !(command == 2 || command == 1) ){
             System.out.println("Invalid Command");
-            m_dataDouble = Double.NaN;
         }
         else {
             //  Parses and returns the double sent by the arduino
