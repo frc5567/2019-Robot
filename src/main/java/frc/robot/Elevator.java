c10fd8f0cd814e97bbc39b0c9a29fb06b63cc87a
patrick-philbin <patrick@philbin.net>
2019-02-05 16:16:20 -0500

    Added stateName variable to the elevator enum and made sure the constructor sets the enum instance currentState to the initial state of LEVEL_ZERO

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -12,52 +12,59 @@ import edu.wpi.first.wpilibj.DigitalInput;
 public class Elevator {
 
     public enum State{
-        LEVEL_ZERO   (0.0 , 1.0 , 1.0),
-        CARGO_L1 (16.75 , 0.80 , 0.50),
-        CARGO_L2 (44.75 , 0.60 , 0.30),
-        CARGO_L3 (72.75 , 0.40 , 0.20),
-        HATCH_L1 (8.25 , 0.90 , 0.50),
-        HATCH_L2 (36.25 , 0.65 , 0.30),
-        HATCH_L3 (64.25 , 0.45 , 0.20);
+		LEVEL_ZERO   (0.0 , 1.0 , 1.0 , "Initial State"),
+		CARGO_L1 (16.75 , 0.80 , 0.50 , "Cargo Level 1"),
+		CARGO_L2 (44.75 , 0.60 , 0.30 , "Cargo Level 2"),
+		CARGO_L3 (72.75 , 0.40 , 0.20 , "Cargo Level 3"),
+		HATCH_L1 (8.25 , 0.90 , 0.50 , "Hatch Level 1"),
+		HATCH_L2 (36.25 , 0.65 , 0.30 , "Hatch Level 2"),
+		HATCH_L3 (64.25 , 0.45 , 0.20 , "Hatch Level 3");
   
-        private double deltaHeightInches;
-        private double maxSpeedPercent;
-        private double maxAngleRate;
+		private double inchesFromInitial;
+		private double maxSpeedPercent;
+		private double maxAngleRate;
+		private String stateName;
   
-        /**
-         * 
-         * 
-         * @param deltaInches Change in height from position zero in inches.
-         * @param maxSpeedModifier Percent of our max speed we use when the elevator is in this state.
-         * @param maxAngleRate How fast our angle motor can move when the elevator is in this state.
-         */
-        State(double deltaInches , double maxSpeedModifier , double maxAngleRate){
-           this.deltaHeightInches = deltaInches;
-           this.maxSpeedPercent = maxSpeedModifier;
-           this.maxAngleRate = maxAngleRate;
-        }
+		/**
+		 * 
+		 * 
+		 * @param inchesFromInitial Change in height from position zero in inches.
+		 * @param maxSpeedModifier Percent of our max speed we use when the elevator is in this state.
+		 * @param maxAngleRate How fast our angle motor can move when the elevator is in this state.
+		 * @param stateName The name of the state associated with the values, in string form.
+		 */
+		State(double inchesFromInitial , double maxSpeedModifier , double maxAngleRate , String stateName){
+		   this.inchesFromInitial = inchesFromInitial;
+		   this.maxSpeedPercent = maxSpeedModifier;
+		   this.maxAngleRate = maxAngleRate;
+		   this.stateName = stateName;
+		}
   
-        /**
-         * @return The change in height from initial to current state.
-         */
-        public double getDeltaHeight(){
-           return this.deltaHeightInches;
-        }
+		/**
+		 * @return The change in height from initial to current state.
+		 */
+		public double getHeightFromInitial(){
+		   return this.inchesFromInitial;
+		}
   
-        /**
-         * @return The max speed modifier based on the elevator's current state.
-         */
-        public double maxSpeedModifier(){
-           return this.maxSpeedPercent;
-        }
+		/**
+		 * @return The max speed modifier based on the elevator's current state.
+		 */
+		public double maxSpeedModifier(){
+		   return this.maxSpeedPercent;
+		}
   
-        /**
-         * @return The max speed we turn our angle arm at the elevator's current state.
-         */
-        public double maxAngleRate(){
-           return this.maxAngleRate;
-        }
-     }
+		/**
+		 * @return The max speed we turn our angle arm at the elevator's current state.
+		 */
+		public double maxAngleRate(){
+		   return this.maxAngleRate;
+		}
+  
+		public String getStateName(){
+		   return this.stateName;
+		}
+	 }
 
     // Defining the limit switches at the top and bottom of the elevator.
     DigitalInput m_limitTop;
@@ -88,7 +95,10 @@ public class Elevator {
         m_elevatorEncoder = new SensorCollection(m_elevatorMotor);
 
         // Zeroes the encoder
-        m_elevatorEncoder.setQuadraturePosition(0, 0);
+		m_elevatorEncoder.setQuadraturePosition(0, 0);
+		
+		//Sets enum to initial state
+		currentState = currentState.LEVEL_ZERO;
     }
 
     /**
