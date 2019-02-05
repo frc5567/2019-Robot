e8bfb6a89cdcffe394d04023774562ce321368aa
HannahStrimpel <h4rescue@gmail.com>
2019-02-04 19:11:50 -0500

    Changes were made to the HatchMech class and DriveTrain class

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -4,28 +4,121 @@
 
 package frc.robot;
 
-//importing the types (speed control, drive, and spark)
+// Imports needed for motor controllers, speed controller groups, and the drivetrain
+import edu.wpi.first.wpilibj.drive.DifferentialDrive; 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.VictorSP;
+// These imports are extending SpeedController, allowing us to use SpeedControllerGroup
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 
-//we imported the methods for the differential drive
+public class Drivetrain {
+
+    // Declares variables for drivetrain speed and rotate setter
+    // Constant is how large each step is in the setters
+    private final double MAX_DELTA_SPEED = 0.1;
+
+    // Declares variables for current speed and rotate rate
+    // Variables used for feedback in speed setter and rotate setter
+    double m_currentSpeed;
+    double m_currentRotate;
+
+    // Declarations for the motor controllers
+    private WPI_VictorSPX m_frontLeftMotor;
+    private WPI_TalonSRX m_frontRightMotor;
+    private WPI_VictorSPX m_backLeftMotor;
+    private WPI_TalonSRX m_backRightMotor;
+
+    // Declarations for the speed controller groups
+    private SpeedControllerGroup m_leftMotors;
+    private SpeedControllerGroup m_rightMotors;
+
+    // Declaration for the drivetrain
+    private DifferentialDrive m_drivetrain;
 
 
-public class DriveTrain {
-    // Declare variable, and set numeric values
-    //We'll eventually make one of the Victors a Talon SRX and have it lead the other
-    //maybe clarify that m_frontLeft is a Victor or Talon
-    VictorSP m_frontLeft = new VictorSP(0);
-    VictorSP m_backLeft = new VictorSP(1);
-    SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_backLeft);
+    /**
+     * Constructor for the motor controller declarations and the drivetrain object
+     * @param frontLeftChannel The channel of the front left motor controller
+     * @param frontRightChannel The channel of the front right motor controller
+     * @param backLeftChannel The channel of the back left motor controller
+     * @param backRightChannel The channel of the back right motor controller
+     */
+    Drivetrain(int frontLeftChannel, int frontRightChannel, int backLeftChannel, int backRightChannel) {
+        
+        // Initializes the motorControllers using the ports passed in
+        m_frontLeftMotor = new WPI_VictorSPX(frontLeftChannel);
+        m_frontRightMotor = new WPI_TalonSRX(frontRightChannel);
+        m_backLeftMotor = new WPI_VictorSPX(backLeftChannel);
+        m_backRightMotor = new WPI_TalonSRX(backRightChannel);
+        
+        // Initializes the motor controller groups (left side motors and right side motors)
+        m_leftMotors = new SpeedControllerGroup(m_frontLeftMotor, m_backLeftMotor);
+        m_rightMotors = new SpeedControllerGroup(m_frontRightMotor, m_backRightMotor);
+        
+        // Initializes the drivetrain with the motor controller groups
+        m_drivetrain = new DifferentialDrive(m_leftMotors, m_rightMotors);
 
-    VictorSP m_frontRight = new VictorSP(2);
-    VictorSP m_backRight = new VictorSP(3);
-    SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_backRight);
+        // Initializes feedback variables for speed setter and rotate setter
+        // Setters use variables as feedback in order to "ramp" the output gradually
+        m_currentSpeed = 0;
+        m_currentRotate = 0;
+    }
 
-    DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
-    
 
+    /**
+     *      || Currently in place if access is needed to DifferentialDrive methods ||
+     *      || Might be used for initSendable or other methods within class ||
+     * Gets the drivetrain object to use DifferentialDrive methods
+     * @return The drivetrain object (DifferentialDrive)
+     */
+    public DifferentialDrive getDrivetrain() {
+        return m_drivetrain;
+    }
+
+    /**
+     * Sets the drivetrain motor to desired settings,
+     * Acceleration limiter is implemented to prevent current spikes
+     * from putting robot in brownout condition
+     * @param desiredSpeed The desired robot speed along x-axis [-1.0..1.0] forward is positive
+     * @param desiredRotate The desired robot turning speed along z-axis [-1.0..1.0] clockwise is positive
+     * @param quickTurn If true, overrides constant-curvature turning for turn-in-place maneuvering
+     */
+    public void curvatureDrive(double desiredSpeed, double desiredRotate, boolean quickTurn) {
+
+        // If desired speed is higher than current speed by a margin larger than kMaxDeltaSpeed,
+        // Increase current speed by kMaxDelaSpeed's amount
+        if (desiredSpeed > (m_currentSpeed + MAX_DELTA_SPEED)) {
+            m_currentSpeed += MAX_DELTA_SPEED;
+        }
+        // If desired speed is less than current speed by a margin larger than kMaxDeltaSpeed
+        // Decrease current speed by kMaxDeltaSpeed's amount
+        else if (desiredSpeed < (m_currentSpeed - MAX_DELTA_SPEED)) {
+            m_currentSpeed -= MAX_DELTA_SPEED;
+        }
+        // If desired speed is within kMaxDeltaSpeed's margin to current speed,
+        // Set current speed to match desired speed
+        else {
+            m_currentSpeed = desiredSpeed;
+        }
+
+        // If desired rotate is higher than current rotate by a margin larger than kMaxDeltaSpeed,
+        // Increase current rotate by kMaxDelaSpeed's amount
+        if (desiredRotate > (m_currentRotate + MAX_DELTA_SPEED)) {
+            m_currentRotate += MAX_DELTA_SPEED;
+        }
+        // If desired rotate is less than current rotate by a margin larger than kMaxDeltaSpeed
+        // Decrease current rotate by kMaxDeltaSpeed's amount
+        else if (desiredRotate < (m_currentRotate - MAX_DELTA_SPEED)) {
+            m_currentRotate -= MAX_DELTA_SPEED;
+        }
+        // If desired rotate is within kMaxDeltaSpeed's margin to current rotate,
+        // Set current rotate to match desired speed
+        else {
+            m_currentRotate = desiredRotate;
+        }
+
+        // Pass current speed, current rotate, and the quick turn boolean into the Differential Drive's curatureDrive method
+        m_drivetrain.curvatureDrive(m_currentSpeed, m_currentRotate, quickTurn);
+    }
 }
--- src/main/java/frc/robot/HatchMech.java
+++ src/main/java/frc/robot/HatchMech.java
@@ -1,37 +1,62 @@
 /*Defines Mechanism that allows hatches to be placed on cargo ships and rockets.
- */
+*/
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.VictorSP;          //Might change VictorSP for motor later
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class HatchMech{
 
     Servo m_servo;
-    
 
-    HatchMech() {
-    //create servo variable for hatch mechanism
-        m_servo = new Servo(1);             //need to add the channel that the servo will be plugged into
+    VictorSP m_hatchArmMotor;
+    DigitalInput m_limitSwitchTop;
+    DigitalInput m_limitSwitchBottom;
+
+        
+    HatchMech(){
+        //create servo variable for hatch mechanism
+        m_servo = new Servo(1);         //need to add the channel that the servo will be plugged into
+
+        m_limitSwitchTop = new DigitalInput(0);
+        m_limitSwitchBottom = new DigitalInput(1);
     }
 
     public void OpenServo(){
-         m_servo.setAngle(0);       //double check on degrees and see if servo is the right type
+        m_servo.setPosition(0.6);            //double check on degrees and see if servo is the right type
     }
 
-    public void ClosedServo(){
-        m_servo.setAngle(90);
+    public void CloseServo(){
+        m_servo.setPosition(0.3);
     }
 
-    public void switchServo(boolean button){
+    public void SwitchServo(boolean button){
         if (button){
-            if (m_servo.getAngle()==0){
-                ClosedServo();
+            if (m_servo.getAngle()==0.6){
+                CloseServo();
             }
             else{
                 OpenServo();
             }
         }
     }
+
+    public void ArmUp(){
+        while(m_limitSwitchTop.get() == false){
+            m_hatchArmMotor.set(0.5);                   // Double check direction and speed
+        }
+        m_hatchArmMotor.set(0.0);
+                               
+    }
+
+    public void ArmDown(){
+        while(m_limitSwitchBottom.get() == false){
+        m_hatchArmMotor.set(-0.5);                       // Double check direction and speed        
+        }
+        m_hatchArmMotor.set(0.0);
+    }
+
 
 }
 
