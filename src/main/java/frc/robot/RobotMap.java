924e19f79ec50a01cb2936b6477ba6731b8b33d9
FallenFlame6289 <mattruff78@gmail.com>
2019-02-09 10:52:26 -0500

    Fixing tab sizes

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,38 +1,39 @@
 package frc.robot;
 
 /**
- * This class is designed to map out the port numbers for motor controllers, sensors,
- * and other objects on the robot that require a dedicated port on the RoboRIO.
+ * This class is designed to map out the port numbers for motor controllers,
+ * sensors, and other objects on the robot that require a dedicated port on the
+ * RoboRIO.
  */
 public class RobotMap {
 
-    // Math constants
-    // NavX angle offset
-    public static final int ANGLE_OFFSET = 180;
+	// Math constants
+	// NavX angle offset
+	public static final int ANGLE_OFFSET = 180;
 
-    // Controllers
-    public static final int PILOT_CONTROLLER_PORT = 0;
-    public static final int COPILOT_CONTROLLER_PORT = 1;
+	// Controllers
+	public static final int PILOT_CONTROLLER_PORT = 0;
+	public static final int COPILOT_CONTROLLER_PORT = 1;
 
-    // CAN Motor controllers
-    // Drivetrain
-    public static final int FRONT_LEFT_DRIVE_MOTOR_PORT = 0;
-    public static final int FRONT_RIGHT_DRIVE_MOTOR_PORT = 1;
-    public static final int BACK_LEFT_DRIVE_MOTOR_PORT = 2;
-    public static final int BACK_RIGHT_DROVE_MOTOR_PORT = 3;
-    // Elevator
-    public static final int ELEVATOR_MOTOR_PORT = 4;
+	// CAN Motor controllers
+	// Drivetrain
+	public static final int FRONT_LEFT_DRIVE_MOTOR_PORT = 0;
+	public static final int FRONT_RIGHT_DRIVE_MOTOR_PORT = 1;
+	public static final int BACK_LEFT_DRIVE_MOTOR_PORT = 2;
+	public static final int BACK_RIGHT_DROVE_MOTOR_PORT = 3;
+	// Elevator
+	public static final int ELEVATOR_MOTOR_PORT = 4;
 
-    // PWM Motor Controllers
-    // Climber
-    public static final int FRONT_CLIMBER_MOTOR_PORT = 0;
-    public static final int BACK_CLIMBER_MOTOR_PORT = 1;
+	// PWM Motor Controllers
+	// Climber
+	public static final int FRONT_CLIMBER_MOTOR_PORT = 0;
+	public static final int BACK_CLIMBER_MOTOR_PORT = 1;
 
-    // Sensors
-    // Elevator limit switches
-    public static final int ELEVATOR_LIMIT_TOP_PORT = 0;
-    public static final int ELEVATOR_LIMIT_BOTTOM_PORT = 1;
-    // Climber limit switches
-    public static final int FRONT_CLIMBER_LIMIT_TOP_PORT = 2;
-    public static final int BACK_CLIMBER_LIMIT_TOP_PORT = 3;
+	// Sensors
+	// Elevator limit switches
+	public static final int ELEVATOR_LIMIT_TOP_PORT = 0;
+	public static final int ELEVATOR_LIMIT_BOTTOM_PORT = 1;
+	// Climber limit switches
+	public static final int FRONT_CLIMBER_LIMIT_TOP_PORT = 2;
+	public static final int BACK_CLIMBER_LIMIT_TOP_PORT = 3;
 }
