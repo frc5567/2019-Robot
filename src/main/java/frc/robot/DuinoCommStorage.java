c226eff68e16580e0bde07802b6d171c02627a96
Josh <josh.overbeek@outlook.com>
2019-02-11 18:21:36 -0500

    Added storage doubles for the low pixy returns

--- src/main/java/frc/robot/DuinoCommStorage.java
+++ src/main/java/frc/robot/DuinoCommStorage.java
@@ -4,5 +4,6 @@ public class DuinoCommStorage {
     //  Storage for the last valid values from the high camera
     double degTargetHigh, distTargetHigh;
     //  Storage for the last valid values from the low camera
+    double positionLow, angleToCenterLow;
 
 }
